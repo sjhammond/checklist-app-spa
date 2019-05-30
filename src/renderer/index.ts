81ea6e22acce28e042414ab006f29ceb5a2c166d
Sam Hammond <sjh@milestone.dk>
2019-05-30 12:09:17 -0500

    css fix

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,11 +1,6 @@
 import { renderMainMenu } from "./functions/menuBuilder";
 import '../../static/css/app.css'
 
-const styles = document.createElement('style');
-styles.setAttribute('type', 'text/css');
-styles.setAttribute('rel', 'stylesheet');
-styles.setAttribute('href', '../static/css/app.css');
-
 const app = document.getElementById('app');
 app.innerHTML = `
 
